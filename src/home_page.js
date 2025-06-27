fb2d8682d10e671cfd0b0b930ce5e679926f484c
Adamnson-0dinson <theawakeavatar@gmail.com>
2025-06-27 18:53:36 +0530

    (feat) added background image to div, added welcome message

--- src/home_page.js
+++ src/home_page.js
@@ -1,7 +1,24 @@
-const home = "This is the Home page";
 const content_div = document.querySelector("#content");
 
 export function showHome() {
-  console.log("home button was clicked");
-  content_div.innerHTML = home;
+
+  content_div.innerHTML = " ";
+  let line1 = document.createElement("p");
+  let line2 = document.createElement("p");
+  let line3 = document.createElement("p");
+  let welcome_div = document.createElement("div"); 
+
+
+  line1.setAttribute('id', "line-1");
+  line2.setAttribute('id', "line-2");
+  line3.setAttribute('id', "line-3");
+  welcome_div.setAttribute("class", "welcome-container");
+  line1.innerHTML = "Welcome to";
+  line2.innerHTML = "Keerti";
+  line3.innerHTML = "Snacks Corner";
+  welcome_div.appendChild(line1);
+  welcome_div.appendChild(line2);
+  welcome_div.appendChild(line3);
+
+  content_div.appendChild(welcome_div);
 }
--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
 import "./page_setup.js";
 import "./navbar.css"
-export * from "./home_page.js";
+import { showHome } from "./home_page.js";
+
+export * from "./home_page.js"
 export * from "./menu_page.js";
 export * from "./contact_us.js";
 export * from "./cart.js";
 
-console.log("webpack setup done");
-// const content_div = document.querySelector("#content");
-// const btn_home = document.querySelector("#home");
-// const btn_menu = document.querySelector("#menu");
-// const btn_contact = document.querySelector("#contact");
-// const btn_cart = document.querySelector("#cart");
-// btn_home.addEventListener('click', showHome())
-// btn_menu.addEventListener('click', showMenu())
-// btn_contact.addEventListener('click', showContact())
-// btn_cart.addEventListener('click', showCart())
-
-
-
-
-
+showHome();
 
 
